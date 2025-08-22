3d41675843a21748c6315bd8b7f1ae3788ba7589
geovanesilvajr <geovane.silvajr@gmail.com>
2025-08-22 19:24:39 -0300

    feat: add Day 3 project - Message Generator

---
+++ aprendendo-javascript/semana-1-fundamentos/dia3.js
@@ -0,0 +1,6 @@
+const cidade = "Belo Horizonte";
+const estado = "Minas Gerais";
+const anoFundacao = "1897";
+const mensagem = `A cidade de ${cidade}, localizada em ${estado}, foi fundada em ${anoFundacao}.`;
+
+console.log(mensagem);

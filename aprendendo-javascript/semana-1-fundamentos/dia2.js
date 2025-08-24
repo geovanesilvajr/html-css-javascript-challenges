f749967a3d20595dd4f14fb81f0fd4d61bc6ea1c
geovanesilvajr <geovane.silvajr@gmail.com>
2025-08-24 07:44:45 -0300

    feat: add Day 1 project - alert button. And change project schedule.

--- aprendendo-javascript/semana-1-fundamentos/dia2.js
+++
@@ -1,8 +0,0 @@
-const altura = 1.75;
-const peso = 68;
-
-const imc = peso / (altura * altura);
-
-console.log(
-  `Com altura de ${altura} e peso de ${peso}, o IMC é ${imc.toFixed(1)}`
-);
---
+++ frontend-practice/semana-1/dia-1/js/script.js
@@ -0,0 +1,10 @@
+const button01 = document.getElementById("btn01");
+const button02 = document.getElementById("btn02");
+
+button01.addEventListener("click", () => {
+  alert("Hello World");
+});
+
+button02.addEventListener("click", () => {
+  alert("Olá mundo");
+});
